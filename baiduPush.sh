5ee44b14e3c206723aaa3ad6fa349cb7e6ee2c59
Fang_冬 <752043344@qq.com>
2023-11-09 17:20:50 +0800

    新增微信对象存储开发文档

--- baiduPush.sh
+++ baiduPush.sh
@@ -8,4 +8,4 @@ node utils/baiduPush.js https://www.utuai.com
 # 百度链接推送
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.utuai.com&token=DHcQIe1wfxi2ZCYx"
 
-rm -rf urls.txt # 删除文件
+# rm -rf urls.txt # 删除文件
